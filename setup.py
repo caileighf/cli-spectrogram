6cf8da6c2d33d3f3b8637776ca1752365a9074dc
caileighf <caileighfitzgerald@gmail.com>
2020-03-04 22:00:03 -0500

    wip

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cli-spectrogram", 
-    version="0.2.1",
+    version="0.2.2",
     author="Caileigh Fitzgerald",
     author_email="cfitzgerald@whoi.edu",
     description="Simple python module that creates spectrograms in the command line",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/caileighf/cli-spectrogram",
-    download_url="https://github.com/caileighf/cli-spectrogram/archive/0.2.tar.gz",
+    download_url="https://github.com/caileighf/cli-spectrogram/archive/0.2.2.tar.gz",
     install_requires=['numpy', 'pathlib'],
     packages=setuptools.find_packages(),
     classifiers=[
