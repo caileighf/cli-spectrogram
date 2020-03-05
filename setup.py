451dc6ee57265e15623489fdf552b8174a91c408
caileighf <caileighfitzgerald@gmail.com>
2020-03-04 21:20:05 -0500

    fixing 0.2 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cli-spectrogram", 
-    version="0.2",
+    version="0.2.1",
     author="Caileigh Fitzgerald",
     author_email="cfitzgerald@whoi.edu",
     description="Simple python module that creates spectrograms in the command line",
